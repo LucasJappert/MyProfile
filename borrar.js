da2543190bb7ff922e1993e1a19649d556d8aba0
Lucas Jappert <lucasnicolasjappert@gmail.com>
2022-07-04 16:43:45 -0300

    update CV

--- borrar.js
+++ borrar.js
@@ -1,4 +1,13 @@
+/*
+
 Hola! Mi nombre es Lucas Jappert. 
 Mi carrera comenzó aproximadamente en el 2008 y siempre me gustó trabajar en backend como en frontend.
 Si bien son muchas las tecnologías con las que he trabajado, hoy en día estoy fascinado con javascript y typescript, utilizando Vuejs para todo lo que implica el frontend y nodejs + express para el backend. Creo que javascript siempre fue una poderosísima herramienta con la cual se puede realizar cualquier tipo de aplicación (escritorio, mobile, API, extensiones para el navegador, etc), y que seguramente tenga todavía un gran futuro.
 Respecto a mi personalidad, soy una persona que puede adaptarse a cualquier tipo de trabajo, como así también aportar grandes valores y conocimientos al equipo.
+
+My career started around 2008 and I always liked working on the backend as well as the frontend.
+Although there are many technologies that I have worked with, today I am fascinated with javascript and typescript, using Vuejs for everything that involves the frontend and nodejs + express for the backend.
+I believe that javascript has always been a very powerful tool with which any type of application can be created (desktop, mobile, API, browser extensions, etc.), and that it surely still has a great future. 
+Regarding my personality, I am a person who can adapt to any type of work, as well as bring great values and knowledge to the team.
+
+*/
